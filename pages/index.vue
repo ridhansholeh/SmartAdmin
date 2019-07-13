1ac07b0a89eba4d3362d820a415395f445e1d5cf
Ridhan Sholeh <ridhannewbiecheaters@gmail.com>
2019-07-13 23:04:48 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,29 @@
 			<li class="breadcrumb-item active">Index</li>
 			<li class="position-absolute pos-top pos-right d-none d-sm-block"><span class="js-get-date"></span></li>
 		</ol>
+
+		<div class="alert alert-primary">
+			<div class="d-flex flex-start w-100">
+				<div class="mr-2 hidden-md-down">
+					<span class="icon-stack icon-stack-lg">
+						<i class="base base-6 icon-stack-3x opacity-100 color-primary-500"></i>
+						<i class="base base-10 icon-stack-2x opacity-100 color-primary-300 fa-flip-vertical"></i>
+						<i class="ni ni-blog-read icon-stack-1x opacity-100 color-white"></i>
+					</span>
+				</div>
+				<div class="d-flex flex-fill">
+					<div class="flex-fill">
+						<span class="h5">Hello my name is Ridhan!</span>
+						<p>This is an example SmartAdmin template with the Nuxt.js Framework.</p>
+					</div>
+				</div>
+			</div>
+		</div>
 	</main>
 </template>
 
 <script>
 	export default {
-
 	}
 </script>
 
